3ac748e2be72b0e6f0b3b8dba651fc1f023d4068
Charlotte <59579574+charlinked@users.noreply.github.com>
2020-06-03 21:24:37 -0700

    webscrapers working version

---
+++ webscrapers/FP_chanel.py
@@ -0,0 +1,169 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+import seaborn as sns
+
+import sys
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QUrl
+import bs4 as bs
+import urllib.request
+
+import csv
+
+'''
+This script scrapes data on CHANEL bags form fashionhpile and writes to a CSV file
+IMPORTANT: use pyqt version 5.12 and nor 5.14 (crashes)
+
+Functions for webpage loading and data retrieval:
+'''
+
+#loading dynamic webpage
+class Page(QWebEnginePage):    
+
+    app = QApplication(sys.argv) # this variable should be global so it doesn't cause the app to crash when looped
+    
+    def __init__(self, url):
+        QWebEnginePage.__init__(self)
+        self.html = ''
+        self.loadFinished.connect(self._on_load_finished)
+        self.load(QUrl(url))
+        self.app.exec_()
+
+    def _on_load_finished(self):
+        self.html = self.toHtml(self.Callable)
+        print('Load finished')
+
+    def Callable(self, html_str):
+        self.html = html_str
+        self.app.quit()
+
+#Get text out of a specific p element in the soup
+def getcontent(pcontains):
+        try:
+            element = (soup2.select_one(pcontains)).getText()
+        except:
+            element = None
+        return element
+
+#Clean a specific element we extracted out of the soup with getcontent()  
+def cleancontent(content,tostrip):
+    if content != None:
+        content = content.strip(tostrip).replace('','')
+    return content    
+
+#Extract seperate measurements #mm = measurem
+def measuremext(mm):
+    (blength, height, width, drop) = (None, None, None, None)
+    mlist = mm.split('in')
+    mlist.pop()
+    
+    # Get Base Length - is always first
+    blength = mlist[0]
+    if 'Length' in blength:
+        blength = blength.split(": ")
+        blength = blength.pop()
+    # Get strap drop - is always last
+    if 'Drop' in mlist[-1]:
+        drop = mlist[-1]
+        drop = (drop.split(": ")).pop()
+    # get height and width - are not always in same position of the list
+    ind = 0
+    while ind < len(mlist):
+        if 'Height' in mlist[ind]:
+            height = mlist[ind]
+            height = (height.split(": ")).pop()
+        elif 'Width' in mlist[ind]:
+            width = mlist[ind]
+            width = (width.split(": ")).pop()
+        ind += 1
+        
+    return (blength, height, width, drop) 
+
+'''
+Webscraping multiple webpages in a loop - saving output to a soup object each time -
+extracting the relevant info, cleaning up the info, casting info to a list - writing list to csv file
+
+Scraping info on all CHANEL bags from Fashionphile
+'''
+
+#Empty list to store content    
+bags = []
+#append the list with the headers for each column - has to be a string, make sure corresponds to variables at the end
+bags.append(['productname','productpage','price','brand','estretailpr','itemnr','category','length','height','width','drop','year',
+'acc_included','description','condition','cond_ext','cond_hw','cond_handle','cond_int','cond_other'])
+
+for i in range(1,8): #Loops over different overview pages 'CHANEL bag' - currently 7 pages
+    pagenr = str(i)
+    urlpage = 'https://www.fashionphile.com/shop/categories/handbag-styles/?brands=chanel&pageSize=180&sort=date-desc&page='+pagenr
+    print(urlpage)
+    # open the URL with pyqt5, store info to Soup object with bs
+    page = Page(urlpage)
+    soup = bs.BeautifulSoup(page.html, 'html.parser')
+    #find all product cards in the soup
+    results = soup.find_all('div',attrs={'class': 'product_container'})
+    #creating a counter that tracks scraping of individual product pages
+    counter = 0
+    print(len(results)) #total number of product pages to scrape
+
+    for result in results: #on each overview page: loops over different product cards to retrieve info of each product
+        counter+=1
+        print(counter)
+
+        # scrape info of 1 product from overview page
+        prname = result.find('h4',attrs={'class': 'product-title'}).getText()
+        prpage = result.find('a').get('href')
+        price = result.find('span',attrs={'class': 'sale-price'}).getText()
+        brand = result.find('h3',attrs={'class': 'brand'}).getText()
+        
+        #pyqt5 - loads individual product page and saves to soup2
+        page2 = Page(prpage)
+        soup2 = bs.BeautifulSoup(page2.html, 'html.parser')
+        
+        # get info out of the soup2 - using my 'getcontent' function defined earlier
+        itemnr = getcontent('p:contains("Item #:")')
+        measurem = getcontent('p:contains("Measurements")')
+        year = getcontent('p:contains("Year:")')
+        acc = getcontent('p:contains("Comes With:")')
+        descr = getcontent('p:contains("We guarantee this is")')
+        cond = getcontent('p:contains("Item Condition:")')
+        excond = getcontent('p:contains("Exterior:")')
+        condhw = getcontent('p:contains("Hardware:")')
+        condhandle = getcontent('p:contains("Handle: ")')
+        condint = getcontent('p:contains("Interior: ")')
+        condother = getcontent('p:contains("Other: ")')
+        try: #get Retail price if present
+            estret = (soup2.find('p',attrs={'class': 'est-retail'}).getText()).strip('Est. Retail: $').replace('','')
+        except:
+            estret = None  
+        category = ((soup2.find('ol',attrs={'class': 'breadcrumb'}).getText()).split("\n\n")).pop(-2)  
+
+        #cleaning content with 'cleancontent' function defined earlier
+        itemnr = cleancontent(itemnr,'Item #: ')
+        (blength, height, width, drop)  = measuremext(measurem) #use predefined function to extract indiv measurements
+        year = cleancontent(year,'Year: ')
+        cond = cleancontent(cond,'Item Condition: ') # this is still being weird - fix
+        excond = cleancontent(excond ,'Exterior: ') 
+        condhw = cleancontent(condhw,'Hardware: ')
+        condhandle = cleancontent(condhandle,'Handle: ')
+        acc = cleancontent(acc,'\nComes With: ')
+        condint = cleancontent(condint,'Interior: ')
+        condother = cleancontent(condother,'Other: ') 
+        category = category[1:]  
+        
+        # append clean content to the list - make sure order corresponds to headers defined above !!
+        bags.append([prname, prpage,price,brand,estret,itemnr,category,blength,height,width,drop,year,
+        acc,descr,cond,excond,condhw,condhandle,condint,condother])
+
+#print(bags) #print output of list
+
+# Create csv and write bags list to output file
+with open('FPChanelbags.csv','w', newline='') as f_output:
+    csv_output = csv.writer(f_output)
+    csv_output.writerows(bags)
+
+print('data saved to file: completed')
---
+++ webscrapers/rebag_gucci.py
@@ -0,0 +1,153 @@
+'''
+import statements
+'''
+import pandas as pd
+import numpy as np
+#used to open URLs
+import urllib.request
+#used to extract data from html files
+import bs4 as bs #use soup = bs.BeautifulSoup(import bs4 as bs #use soup = bs.BeautifulSoup(
+from requests import get
+import csv# import statements
+
+import sys
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QUrl# import statements
+#needed to write from print function
+import io
+
+'''
+Global functions
+'''
+#loading dynamic webpage
+class Page(QWebEnginePage):    
+
+    app = QApplication(sys.argv) # this variable should be global so it doesn't cause the app to crash when looped
+    
+    def __init__(self, url):
+        QWebEnginePage.__init__(self)
+        self.html = ''
+        self.loadFinished.connect(self._on_load_finished)
+        self.load(QUrl(url))
+        self.app.exec_()
+
+    def _on_load_finished(self):
+        self.html = self.toHtml(self.Callable)
+        print('Load finished')
+
+    def Callable(self, html_str):
+        self.html = html_str
+        self.app.quit()
+
+#function to get all info from the product page into contents
+def print_to_string(el): 
+    output = io.StringIO()
+    print(el, file=output)
+    contents = output.getvalue()
+    output.close()
+    return contents
+#function to get seperate elements out of content
+def getcontentout(string1, string2):
+    try:
+        thecontent = ((piece2.split(string1)[1]).split(string2))[0]
+        return thecontent
+    except:
+        return None
+
+'''
+Scrapes all overview pages in a loop for Gucci bags - 16 pages currently
+'''
+
+bags = []
+bags.append(['productname','brand','price','pagelink','condition','conditiondetails','exteriorcolor','exteriormaterial','hardware','accessories','brandcode', 'measurements','itemnr','claircode','fulldescr'])
+
+for i in range(1,17): 
+    pagenr = str(i)
+    urlpage = 'https://shop.rebag.com/collections/Gucci/?page='+pagenr
+    print(urlpage)
+    req = urllib.request.Request(urlpage)
+    # providing a basic user-agent so our scraper doesn't get blocked
+    # query the website and return the html to the variable 'page'
+    req.add_header('User-Agent', 'Mozilla/5.0')
+    page = urllib.request.urlopen(req)
+    soup = bs.BeautifulSoup(page, 'html.parser')
+
+    #in the soup find all product cards
+    results = soup.find_all('div',attrs={'class': 'product-caption'})
+    #check if the results we got corresponds to the number of elements on a page
+    print('Number of results', len(results))#in the soup find all product cards 
+    counter = 0
+
+    '''
+    Scrapes each product on the overview page 
+    '''
+    for i in range(len(results)):
+        counter+=1
+        print(counter) 
+
+        #go per item/line of results
+        data = results[i]
+        
+        #extract seperate elements
+        page2 = data.find('a',attrs={'class': 'product-price'}).get('href')
+        price = data.find('a',attrs={'class': 'product-price'}).getText()
+        brand = data.find('a',attrs={'class': 'product-vendor'}).getText()
+
+        #go to product page and scrape productpage into soup2
+        url2 ='https://shop.rebag.com'+page2
+        nextpage = Page(url2)
+        soup2 = bs.BeautifulSoup(nextpage.html, 'html.parser')
+        #get info out of soup2, is in javascript within <script> tag
+        elements = soup2.find('script',attrs={'data-creator_name': 'Rebag','type': 'application/ld+json'})
+        #use function to dump javascript content into a string 
+        description_str = print_to_string(elements)
+        #use string and list manipulations to extract info seperately
+        try:
+            list1 = description_str.split("Condition: ")
+        except:
+            list1 = None
+        try:
+            piece2 = list1[1]
+        except:
+             piece2 = None
+
+        #extracts condition and condition details
+        try:
+            condition = (piece2.split("."))[0]
+        except:
+            condition = None
+        try: 
+            conditiondet = ((piece2.split("  Accessories:"))[0]).strip(condition)
+        except:
+            conditiondet = None
+        #use function to extract all other elements seperately
+        acc = getcontentout('Accessories: ', '  Measurements:')
+        measurem = getcontentout('Measurements: ', ' Designer: ')
+        model = getcontentout('Gucci Model: ',' Exterior Material:')  #change for other designers!
+        extmat = getcontentout('Exterior Material: ',' Exterior Color:') 
+        extcolor = getcontentout('Exterior Color: ',' Interior Material:')
+        hardwarecolor = getcontentout('Hardware Color: ',' Brand Code:')
+        authcode = getcontentout('Brand Code: ',' Clair Code:')
+        itemnr = getcontentout('Item Number: ','",\n        "image":')
+        claircode = getcontentout('Clair Code: ','Item Number: ')
+        try:
+            fulldescr= ((description_str.split('"description": "')[1]).split('. These are professional pictures'))[0]
+        except:
+            fulldescr= None
+
+        #append list with info of each bag
+        bags.append([model,brand,price,page2,condition,conditiondet,extcolor,extmat,hardwarecolor,acc,authcode,measurem,itemnr,claircode,fulldescr])
+        print(bags[-1])
+
+#print(bags)
+
+# Create csv and write bags list to output file
+with open('rebag_gucci.csv','w', newline='') as f_output:
+    csv_output = csv.writer(f_output)
+    csv_output.writerows(bags)
+
+print('data saved to file: completed')
+
+
